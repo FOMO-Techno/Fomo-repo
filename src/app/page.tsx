0591cd98150d5753db71eea6248c20f7b36dd5ef
Rawand-abu-shammala <rwndsami@gmail.com>
2025-05-12 16:05:50 +0300

    feat:The pages.tsx page was not uploaded with them and during the upload, files were deleted from the project, so I added them later.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
-import { ProfileCircle } from "@/components/icons"
-import { Button } from "@/components/ui/button"
+'use client'
 
-export default function Home() {
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+
+export default function LandingPage() {
   return (
-    <div>
-      {/* <Button>Click me</Button> */}
-      <span className="text-lg font-bold text-primary">
-      <ProfileCircle />
-      </span>
-    </div>
+    <main className="min-h-screen flex flex-col items-center justify-center bg-gray-50 space-y-6">
+      <h1 className="text-4xl font-bold">Welcome to FOMO</h1>
+      <p className="text-gray-600">Please choose an option to continue</p>
+      <div className="flex space-x-4">
+        <Button className=' bg-blue-600' variant="outline" size="lg" asChild>
+          <Link href="/signup">Sign Up</Link>
+        </Button>
+        <Button size="lg" asChild>
+          <Link href="/login">Sign In</Link>
+        </Button>
+      </div>
+    </main>
   )
 }
