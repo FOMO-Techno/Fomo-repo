eb6caef5846d011218eed7f21a8413a567bf2b08
hebamesluh1 <hebamesluh@gmail.com>
2025-05-16 11:24:25 +0300

    WIP

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,16 +1,16 @@
 // app/home/page.tsx
-import { HeroSection } from '../components/HeroSection'
-import { ResourcesGrid } from '../components/ResourcesGrid'
+// import { HeroSection } from '../components/HeroSection'
+// import { ResourcesGrid } from '../components/ResourcesGrid'
 
 
 
 export default function HomePage() {
   return (
     <>
-      <HeroSection />
+      {/* <HeroSection /> */}
       <section className="max-w-7xl mx-auto px-4 py-12">
         <h2 className="text-2xl font-semibold mb-6">Explore Learning Paths And Tech Resources</h2>
-        <ResourcesGrid />
+        {/* <ResourcesGrid /> */}
       </section>
     </>
   )
